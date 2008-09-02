412796c8f188c6476d235100f3a4c56153130e13
Rob Ennals <rob@ennals.org>
2008-09-02 11:44:21 -0700

    Improved icons for margin open/close tab

--- client_js/tl_robconfig.js
+++ client_js/tl_robconfig.js
@@ -1,5 +1,5 @@
 var thinklink_urlbase = "http://mashmaker.intel-research.net/rob/server/";
-thinklink_imagebase = "http://mashmaker.intel-research.net/beth/images/";
+thinklink_imagebase = "http://mashmaker.intel-research.net/rob/images/";
 var thinklink_pointbase = "http://mashmaker.intel-research.net:3000/points/showmini/";
 var thinklink_mainhome = "http://mashmaker.intel-research.net:3000/";
 // var thinklink_mainhome = "http://localhost:3000/";
