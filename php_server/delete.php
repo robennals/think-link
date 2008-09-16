1206471706d7425d8d2c6ed020e6378eea3aed68
Rob Ennals <rob@ennals.org>
2008-09-16 16:54:29 -0700

    File missing from previous commit

--- php_server/delete.php
+++ php_server/delete.php
@@ -1,3 +1,5 @@
+<?php
+
 //  Copyright 2008 Intel Corporation
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,8 +14,6 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-
-<?php
 
 require_once 'common.php';
 
@@ -31,43 +31,43 @@ $id = postarg("id");
 
 if($cls == "Topic"){
 	$oldrow = sql_to_array("SELECT * FROM topics WHERE id = '$id';");
-	$data = json_encode($oldrow);
+	$data = addslashes(json_encode($oldrow));
 	sql_query("INSERT INTO history (user_id,table_name,logaction,row_id,json_data) VALUES ($user,'topics','remove','$id','$data');");
 	sql_query("DELETE FROM topics WHERE id = '$id';");
 	
 	$oldlinks = sql_to_array("SELECT * FROM topic_links WHERE parent_id = '$id' OR child_id = '$id' ;");
-	$data = json_encode($oldlinks);
+	$data = addslashes(json_encode($oldlinks));
 	sql_query("INSERT INTO history (user_id,table_name,logaction,row_id,json_data) VALUES ($user,'topics-topic_links','unlink','$id','$data');");
 	sql_query("DELETE FROM topic_links WHERE parent_id = '$id' OR child_id = '$id';");
 
 	$oldlinks = sql_to_array("SELECT * FROM point_topics WHERE topic_id = '$id';");
-	$data = json_encode($oldlinks);
+	$data = addslashes(json_encode($oldlinks));
 	sql_query("INSERT INTO history (user_id,table_name,logaction,row_id,json_data) VALUES ($user,'topics-point_topics','unlink','$id','$data');");
 	sql_query("DELETE FROM point_topics WHERE topic_id = '$id';");
 
 }else if($cls == "Point"){
 	$oldrow = sql_to_array("SELECT * FROM points WHERE id = '$id';");
-	$data = json_encode($oldrow);
+	$data = addslashes(json_encode($oldrow));
 	sql_query("INSERT INTO history (user_id,table_name,logaction,row_id,json_data) VALUES ($user,'points','remove','$id','$data');");
 	sql_query("DELETE FROM points WHERE id = '$id';");
 	
 	$oldlinks = sql_to_array("SELECT * FROM point_links WHERE point_a_id = '$id' OR point_b_id = '$id' ;");
-	$data = json_encode($oldlinks);
+	$data = addslashes(json_encode($oldlinks));
 	sql_query("INSERT INTO history (user_id,table_name,logaction,row_id,json_data) VALUES ($user,'points-point_links','unlink','$id','$data');");
 	sql_query("DELETE FROM point_links_links WHERE point_a_id = '$id' OR point_b_id = '$id';");
 
 	$oldlinks = sql_to_array("SELECT * FROM point_topics WHERE point_id = '$id';");
-	$data = json_encode($oldlinks);
+	$data = addslashes(json_encode($oldlinks));
 	sql_query("INSERT INTO history (user_id,table_name,logaction,row_id,json_data) VALUES ($user,'points-point_topics','unlink','$id','$data');");
 	sql_query("DELETE FROM point_topics WHERE point_id = '$id';");
 	
 	$oldlinks = sql_to_array("SELECT * FROM point_topics WHERE point_id = '$id';");
-	$data = json_encode($oldlinks);
+	$data = addslashes(json_encode($oldlinks));
 	sql_query("INSERT INTO history (user_id,table_name,logaction,row_id,json_data) VALUES ($user,'points-point_topics','unlink','$id','$data');");
 	sql_query("DELETE FROM point_topics WHERE point_id = '$id';");
 	
 	$oldlinks = sql_to_array("SELECT * FROM snippets WHERE point_id = '$id';");
-	$data = json_encode($oldlinks);
+	$data = addslashes(json_encode($oldlinks));
 	sql_query("INSERT INTO history (user_id,table_name,logaction,row_id,json_data) VALUES ($user,'points-snippets','unlink','$id','$data');");
 	sql_query("DELETE FROM snippets WHERE point_id = '$id';");
 }
