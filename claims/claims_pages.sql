b5a261920708148d0303bc454e3e069827dbee56
John-Mark Agosta <johnmark@ublek.(none)>
2010-01-05 16:26:47 -0800

    create claims db, with examples of one record added to each table.

--- claims/claims_pages.sql
+++ claims/claims_pages.sql
@@ -15,8 +15,8 @@ CREATE TABLE claim
     /* = 1 e.g. "is disputed that"
        = 2 e.g. "global warming exists"
      */
-    , claim_id  INT          NOT NULL
-    , page_id   INT          NOT NULL
+    , claim_id  INT          NOT NULL -- hash of claim
+    , page_id   INT          DEFAULT NULL
     , PRIMARY KEY (claim_id)
     );
 
@@ -24,8 +24,8 @@ DROP TABLE IF EXISTS site;
 
 CREATE TABLE site 
     (url_root  CHAR(36) DEFAULT NULL
-    , site_id  INT      NOT NULL
-    , claim_id INT      NOT NULL
+    , site_id  INT          NOT NULL -- hash of url_root
+    , page_id  INT          DEFAULT NULL
     , PRIMARY KEY (site_id)
     );
 
@@ -40,13 +40,44 @@ CREATE TABLE page
     , content_file   VARCHAR(255)  DEFAULT NULL
     , retrieval_time DATETIME      DEFAULT NULL
     , title          VARCHAR(255)  DEFAULT NULL
-    , rank           INT           DEFAULT -1
+    , rank           INT UNSIGNED  DEFAULT 100000000
     , paragraphs     TEXT          DEFAULT NULL
-    , page_id        INT           NOT NULL
-    , claim_id       INT           NOT NULL
-    , site_id        INT           NOT NULL
+    , page_id        INT           NOT NULL     -- hash of path_str
+    , claim_id       INT           DEFAULT NULL
+    , site_id        INT           DEFAULT NULL
     , PRIMARY KEY (page_id)
     );
 
-/* EOF */
+/* Add one test entry. */
+
+INSERT INTO claim VALUES('All is flux'
+    , 2
+    , 632959151
+    , -1516561284
+    );
+
+INSERT INTO page VALUES ('Writing/all_is_flux.html'
+    , 'all_is_flux.html'
+    , '2010-01-05 15:59:59'
+    , 'ALL IS FLUX'
+    , 1
+    , 'FIRE INTO WATER. Water into Earth. Earth into water. Water into fire. No man can step into the same river twice, for other waters are ever flowing onto him.'
+    , -1516561284
+    , 632959151
+    , 789401766
+    );
+
+INSERT INTO site VALUES('http://www.goines.net/'
+    , 789401766
+    , -1516561284
+    );
+
+/* Here's an example retrieval:
+ * SELECT CONCAT(s.url_root, p.path_str)
+    , p.title
+    , c.claim 
+   FROM site AS s JOIN page AS p USING(site_id) 
+    JOIN claim AS c USING(claim_id);
+ *
+EOF */
 
