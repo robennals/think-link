afdd5238025f145f24a93b82b2407e7fdd470a77
Rob Ennals <rob@rob.(none)>
2010-01-30 23:09:41 -0800

    Seeing if I can quickly create a reliable statistical classifier

--- web_claim_finder/accuracy_stats.py
+++ web_claim_finder/accuracy_stats.py
@@ -10,18 +10,15 @@ From this, get figures for precision and recall.
 import drop_bad_claims as d
 import sys
 
-def get_sets(goodfile,allfile):
+def get_human_sets(goodfile,allfile):
 	humangood = set()
-	autogood = set()
 	all = set()
 	for line in file(goodfile):
 		humangood.add(line)
 	for line in file(allfile):
 		all.add(line)
-		if(d.trim_statement(line)):
-			autogood.add(line)
-	agreed = autogood.intersection(humangood)				
-	return (humangood,autogood,agreed)
+	humanbad = all - humangood
+	return (humangood,humanbad,all)
 
 def main(args):
 	allfile = args[1]
@@ -35,9 +32,12 @@ def main(args):
 		all.add(line)
 		if(d.trim_statement(line)):
 			autogood.add(line)
-	agreed = autogood.intersection(humangood)				
-	print "recall = ",float(len(agreed))/len(humangood)
-	print "precision = ",float(len(agreed))/len(autogood)
+	agreegood = autogood.intersection(humangood)	
+	agreebad = (all - autogood).intersection(all - humangood)
+	agree = agreegood.union(agreebad)
+	print "recall = ",float(len(agreegood))/len(humangood)
+	print "precision = ",float(len(agreegood))/len(autogood)
+	print "accuracy = ",float(len(agree))/len(all)
 	print "good as judged by human = ",float(len(humangood))/len(all)
 	print "good as judged by algorithm = ",float(len(autogood))/len(all)
 	
--- web_claim_finder/classify_good.py
+++ web_claim_finder/classify_good.py
@@ -8,8 +8,47 @@ We use a simple statistical classifier to do this, based on hand-chosen features
 """
 
 import nltk
+import claimfinder as cf
+import accuracy_stats as a
+import random
+import os
 
 def features(claim):
+	features = {}
 	words = nltk.word_tokenize(claim)
-	wordspresent = ["has-"+word for word in words]
-	firstword = ["first-"+words[0]]
+	for word in words: features["has-"+word] = True
+	if len(words) > 0:
+		features["first"] = words[0]
+	if len(words) > 1:
+		features["second"] = words[1]
+	features["length"] = len(words)
+	return features
+		
+def get_training_data():
+	human_marked = []
+	for phrase in cf.phrases:
+		basename = "../training/"+phrase.replace(" ","_")
+		humangood = basename+".manual_good"
+		allfile = basename+".pickedclaims"
+		if os.path.exists(humangood):
+			(good,bad,all) = a.get_human_sets(humangood,allfile)		
+			for claim in good:
+				human_marked.append((claim,True))
+			for claim in bad:
+				human_marked.append((claim,False))
+	random.shuffle(human_marked)
+	return human_marked
+	
+def get_featured_data():
+	annotated = get_training_data()
+	featuresets = [(features(claim),g) for (claim,g) in annotated]
+	length = len(featuresets)
+	splitpoint = int(length * 0.8)
+	train_set,test_set = featuresets[:splitpoint],featuresets[splitpoint:]
+	return (train_set,test_set)
+	
+def test_classifier():
+	(train_set,test_set) = get_featured_data()
+	classifier = nltk.NaiveBayesClassifier.train(train_set)	
+	print "accuracy = "+nltk.classify.accuracy(classifier,test_set)
+	
